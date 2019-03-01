7546a6a0ad62caee940ca8c1a3ed2b40f3fa8522
Ali Younes <ay20-5-1994@hotmail.com>
2019-03-01 16:33:34 +0300

    MCL4

--- MClocalization.py
+++ MClocalization.py
@@ -42,8 +42,8 @@ class ParticleFilter(object):
     def __init__(self):
 
         self.num_particles=100
-        self.start_x=250
-        self.start_y=1360
+        self.start_x=300
+        self.start_y=1250
         #second robot x=250 y=1660
         self.start_theta=0.0
 
@@ -70,19 +70,19 @@ class ParticleFilter(object):
 
     def move_particles(self,dx,dy,dtheta):
 
-        #x_noise = np.random.normal(0, self.distance_noise/4, self.num_particles)
-        move_x=dx+#x_noise
-        #y_noise = np.random.normal(0, self.distance_noise/4, self.num_particles)
-        move_y=dy+#y_noise
-        #angle_noise = np.random.normal(0, self.angle_noise/2, self.num_particles)
-        move_theta=dtheta#+angle_noise
+        x_noise = np.random.normal(0, self.distance_noise/4, self.num_particles)
+        move_x=dx+x_noise
+        y_noise = np.random.normal(0, self.distance_noise/4, self.num_particles)
+        move_y=dy+y_noise
+        angle_noise = np.random.normal(0, self.angle_noise/2, self.num_particles)
+        move_theta=dtheta+angle_noise
         move_point=np.array([move_x,move_y,move_theta]).T
         self.particles = cvt_local2global(move_point, self.particles)
         self.particles[self.particles[:, 1] > 2000 - 100, 1] = 2000 - 100
         self.particles[self.particles[:, 1] < 0, 1] = 0
         self.particles[self.particles[:, 0] > 3000 - 100, 0] = 3000 - 100
         self.particles[self.particles[:, 0] < 100, 0] = 100
-
+        self.particles[self.particles[:, 2] > (2*np.pi) ] %=(2*np.pi)
 
     def calc_weights(self,ranges,intens,angles):
 
@@ -257,13 +257,17 @@ class Montecarlo(object):
                                self.last_odom.pose.pose.orientation.y,
                                self.last_odom.pose.pose.orientation.z,
                                self.last_odom.pose.pose.orientation.w])
+
             rot_last = tr.quaternion_matrix(q_last)[0:3, 0:3]
+
             p_last_curr = rot_last.transpose().dot(p_curr - p_last)
             q_last_curr = tr.quaternion_multiply(tr.quaternion_inverse(q_last), q_curr)
+
             _, _, diff = tr.euler_from_quaternion(q_last_curr)
-            self.dtheta += diff
-            self.dx += (p_last_curr[0]) * 1000
-            self.dy += (p_last_curr[1]) * 1000
+
+            self.dtheta = diff% (2*np.pi)
+            self.dx = (p_last_curr[0]) * 1000
+            self.dy = (p_last_curr[1]) * 1000
 
     def odom_callback(self,msg):
         self.mutex.acquire()
